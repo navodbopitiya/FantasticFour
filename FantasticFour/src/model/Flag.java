6a4cc5348925edb72077493a148483225059a859
Navod Bopitiya <s3617221@student.rmit.edu.au>
2018-03-13 22:34:05 +1100

    Initial Commit V3
    Added initial skeleton code

--- FantasticFour/src/model/Flag.java
+++ FantasticFour/src/model/Flag.java
@@ -8,5 +8,24 @@ package model;
  *
  */
 public class Flag {
+	
+	private boolean FlagTeam, flagState;
+
+	public boolean isFlagTeam() {
+		return FlagTeam;
+	}
+
+	public void setFlagTeam(boolean flagTeam) {
+		FlagTeam = flagTeam;
+	}
+
+	public boolean isFlagState() {
+		return flagState;
+	}
+
+	public void setFlagState(boolean flagState) {
+		this.flagState = flagState;
+	}
+	
 
 }
--- FantasticFour/src/model/Square.java
+++ FantasticFour/src/model/Square.java
@@ -8,5 +8,36 @@ package model;
  *
  */
 public class Square {
+	private int positionX, positionY;
+	private boolean occupied;
+	private int size;
+	private boolean colour;
+	
+	public int getPositionX() {
+		return positionX;
+	}
 
+	public void setPositionX(int positionX) {
+		this.positionX = positionX;
+	}
+ 
+	public int getPositionY() {
+		return positionY;
+	}
+
+	public void setPositionY(int positionY) {
+		this.positionY = positionY;
+	}
+
+	public boolean isOccupied() {
+		return occupied;
+	}
+
+	public void setOccupied(boolean occupied) {
+		this.occupied = occupied;
+	}
+
+	public void draw(){
+		
+	}
 }
