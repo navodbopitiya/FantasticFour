22663e89da2ff4e08b86633ccf8e471c1f896d0b
Navod Bopitiya <s3617221@student.rmit.edu.au>
2018-03-24 14:53:20 +1100

    Revert "the first version"
    This reverts commit bd9ba87cfef6ea1442795d60c5dc206803b7f524.

---
+++ FantasticFour/src/model/Flag.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+package model;
+
+/**
+ * @author Navod Bopitiya
+ *
+ */
+public class Flag {
+	
+	private boolean FlagTeam, flagState;
+
+	public boolean isFlagTeam() {
+		return FlagTeam;
+	}
+
+	public void setFlagTeam(boolean flagTeam) {
+		FlagTeam = flagTeam;
+	}
+
+	public boolean isFlagState() {
+		return flagState;
+	}
+
+	public void setFlagState(boolean flagState) {
+		this.flagState = flagState;
+	}
+	
+
+}
---
+++ FantasticFour/src/model/Player.java
@@ -0,0 +1,48 @@
+/**
+ * 
+ */
+package model;
+
+/**
+ * @author Navod Bopitiya
+ *
+ */
+public class Player {
+
+	private String playername;
+	private Piece[] pieces;
+	
+	public String getPlayername() {
+		return playername;
+	}
+	public void setPlayername(String playername) {
+		this.playername = playername;
+	}
+	public Piece[] getPieces() {
+		return pieces;
+	}
+	public void setPieces(Piece[] pieces) {
+		this.pieces = pieces;
+	}
+	
+	public Piece selectPiece(){
+		return null;
+		
+	}
+	
+	public void setPiece(){
+		
+	}
+	
+	public void movePiece(){
+		
+	}
+	
+	public void attackPiece(){
+		
+	}
+	
+	public void captureFlag(){
+		
+	}
+}
